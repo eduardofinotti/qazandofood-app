68f9605c69087a5020181a879e1785b726a406e1
Eduardo Finotti <eduardo.finotti@jamesdelivery.com.br>
2022-01-25 12:38:04 -0300

    commit

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,114 +1,113 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Image, TextInput, Text, } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import React, {useState} from 'react';
+import {StyleSheet, View, Image, TextInput, Text} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 
 const axios = require('axios');
 
-const logo = require('../../assets/logo.png')
+const logo = require('../../assets/logo.png');
 
-export default function Login({ navigation }) {
+export default function Login({navigation}) {
+  const [ip, setIp] = useState('');
+  const [user, setUser] = useState('contato@qazando.com.br');
+  const [password, setPassword] = useState('123');
 
-    const [ip, setIp] = useState('');
-    const [user, setUser] = useState('');
-    const [password, setPassword] = useState('');
+  async function login() {
+    await axios
+      .post(`http://${ip}:9999/qazando-login`, {
+        username: user,
+        password: password,
+      })
+      .then(function (response) {
+        console.log(response.status);
+        navigation.navigate('Home');
+      })
+      .catch(function (error) {
+        alert(error.response.data.message);
+      });
+  }
 
-    async function login() {
+  useEffect(async () => {
+    const myIp = await AsyncStorage.getItem('@ip');
+    setIp(myIp);
+  }, []);
 
-        await axios.post(`http://${ip}:9999/qazando-login`, {
-            username: user,
-            password: password
-        })
-            .then(function (response) {
-                console.log(response.status);
-                navigation.navigate('Home')
-            })
-            .catch(function (error) {
-                alert(error.response.data.message)
-            });
-    }
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={logo}></Image>
 
-    useEffect(async () => {
-        const myIp = await AsyncStorage.getItem('@ip')
-        setIp(myIp)
-    }, [])
+      <View style={styles.contentInput}>
+        <Text style={styles.labelInput}>Usuário</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="contato@qazando.com"
+          value={user}
+          onChangeText={text => {
+            setUser(text);
+          }}
+        />
 
-    return (
-        <View style={styles.container}>
+        <Text style={[styles.labelInput, {marginTop: 20}]}>Senha</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="********"
+          value={password}
+          onChangeText={text => {
+            setPassword(text);
+          }}
+        />
 
-            <Image style={styles.logo} source={logo}></Image>
-
-            <View style={styles.contentInput}>
-                <Text style={styles.labelInput}>Usuário</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="contato@qazando.com"
-                    value={user}
-                    onChangeText={(text) => { setUser(text) }}
-                />
-
-                <Text style={[styles.labelInput, { marginTop: 20 }]}>Senha</Text>
-                <TextInput style={styles.input}
-                    placeholder="********"
-                    value={password}
-                    onChangeText={(text) => { setPassword(text) }}
-                />
-
-                <TouchableOpacity style={styles.buttonEnter} onPress={() => login()}>
-                    <Text style={styles.buttonEnterText}>ENTRAR</Text>
-                </TouchableOpacity>
-            </View>
-
-        </View>
-    );
+        <TouchableOpacity style={styles.buttonEnter} onPress={() => login()}>
+          <Text style={styles.buttonEnterText}>ENTRAR</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#D84559',
+  },
 
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#D84559'
-    },
+  logo: {
+    width: 150,
+    height: 150,
+  },
 
-    logo: {
-        width: 150,
-        height: 150
-    },
+  contentInput: {
+    justifyContent: 'center',
+    paddingHorizontal: 15,
+    width: '80%',
+    marginTop: 50,
+  },
 
-    contentInput: {
-        justifyContent: 'center',
-        paddingHorizontal: 15,
-        width: '80%',
-        marginTop: 50
-    },
+  labelInput: {
+    marginBottom: 6,
+  },
 
-    labelInput: {
-        marginBottom: 6,
-    },
+  input: {
+    height: 40,
+    borderRadius: 8,
+    backgroundColor: '#fff',
+    paddingHorizontal: 8,
+  },
 
-    input: {
-        height: 40,
-        borderRadius: 8,
-        backgroundColor: '#fff',
-        paddingHorizontal: 8
-    },
+  buttonEnter: {
+    height: 40,
+    borderRadius: 8,
+    backgroundColor: '#FF8B9B',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
 
-    buttonEnter: {
-        height: 40,
-        borderRadius: 8,
-        backgroundColor: '#FF8B9B',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 40
-    },
-
-    buttonEnterText: {
-        color: '#fff',
-        fontWeight: 'bold'
-    }
-
+  buttonEnterText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
 });
-
--- src/screens/Onboarding/index.js
+++ src/screens/Onboarding/index.js
@@ -1,101 +1,99 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Image, TextInput, Text } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import React, {useState} from 'react';
+import {StyleSheet, View, Image, TextInput, Text} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const logo = require('../../assets/logo.png')
-const next = require('../../assets/next.png')
+const logo = require('../../assets/logo.png');
+const next = require('../../assets/next.png');
 
-export default function Onboarding({ navigation }) {
+export default function Onboarding({navigation}) {
+  const [ip, setIp] = useState('192.168.15.40');
 
-    const [ip, setIp] = useState('');
-
-    async function goToLogin() {
-        if (ip != '' && ip.includes('.')) {
-            await AsyncStorage.setItem('@ip', ip)
-            navigation.navigate('Login')
-        } else {
-            alert('Preencha um IP válido.')
-        }
-
+  async function goToLogin() {
+    if (ip != '' && ip.includes('.')) {
+      await AsyncStorage.setItem('@ip', ip);
+      navigation.navigate('Login');
+    } else {
+      alert('Preencha um IP válido.');
     }
+  }
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.titleContainer}>
-                <Text style={styles.title}>Por favor, digite o IP da sua rede!</Text>
-            </View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Por favor, digite o IP da sua rede!</Text>
+      </View>
 
-            <View style={styles.contentInput}>
-                <TextInput
-                    style={styles.input}
-                    placeholder="192.188.10.8"
-                    value={ip}
-                    onChangeText={(text) => { setIp(text) }}
-                />
+      <View style={styles.contentInput}>
+        <TextInput
+          style={styles.input}
+          placeholder="192.188.10.8"
+          value={ip}
+          onChangeText={text => {
+            setIp(text);
+          }}
+        />
 
-                <TouchableOpacity style={styles.buttonEnter} onPress={() => goToLogin()}>
-                    <Image source={next} />
-                </TouchableOpacity>
-            </View>
-
-        </View>
-    );
+        <TouchableOpacity
+          style={styles.buttonEnter}
+          onPress={() => goToLogin()}>
+          <Image source={next} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#D84559',
+  },
 
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#D84559'
-    },
+  titleContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    width: '80%',
+  },
 
-    titleContainer: {
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-        width: '80%'
-    },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
 
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: '#fff'
-    },
+  contentInput: {
+    justifyContent: 'center',
+    marginTop: 50,
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+  },
 
-    contentInput: {
-        justifyContent: 'center',
-        marginTop: 50,
-        flexDirection: 'row',
-        alignItems: 'center',
-        width: '100%'
-    },
+  labelInput: {
+    marginBottom: 6,
+  },
 
-    labelInput: {
-        marginBottom: 6,
-    },
+  input: {
+    height: 40,
+    borderRadius: 8,
+    backgroundColor: '#fff',
+    paddingHorizontal: 8,
+    width: '60%',
+  },
 
-    input: {
-        height: 40,
-        borderRadius: 8,
-        backgroundColor: '#fff',
-        paddingHorizontal: 8,
-        width: '60%'
-    },
+  buttonEnter: {
+    height: 40,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+  },
 
-    buttonEnter: {
-        height: 40,
-        borderRadius: 8,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginLeft: 10
-    },
-
-    buttonEnterText: {
-        color: '#fff',
-        fontWeight: 'bold'
-    }
-
+  buttonEnterText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
 });
-
